692ccecadddc6cf27d879781ce6d9d7d189d05f3
Shubham Agrawal <agshubh191@gmail.com>
2025-06-14 17:40:02 -0700

    Add RIC project page

---
+++ static/js/index.js
@@ -0,0 +1,57 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+window.HELP_IMPROVE_VIDEOJS = false;
+
+
+$(document).ready(function() {
+    // Check for click events on the navbar burger icon
+
+    var options = {
+			slidesToScroll: 3,
+			slidesToShow: 3,
+			loop: true,
+			infinite: true,
+			autoplay: false,
+			autoplaySpeed: 5000,
+    }
+
+		// Initialize all div with carousel class
+    var carousels = bulmaCarousel.attach('.carousel', options);
+	
+    bulmaSlider.attach();
+
+})
+
+
+}
+/*
+     FILE ARCHIVED ON 19:57:35 Jan 05, 2024 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 00:34:28 Jun 15, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.547
+  exclusion.robots: 0.02
+  exclusion.robots.policy: 0.011
+  esindex: 0.011
+  cdx.remote: 5.099
+  LoadShardBlock: 160.729 (3)
+  PetaboxLoader3.datanode: 108.704 (5)
+  PetaboxLoader3.resolve: 205.157 (3)
+  load_resource: 182.415
+  loaddict: 51.082
+*/
